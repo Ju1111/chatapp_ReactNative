2abf8db5a5139e8b1bca416ba1fc412fe0cf74e1
Ju111 <SchneiderJu@gmx.net>
2018-06-04 21:19:34 +0200

    setup router

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { View } from 'react-native';
 import SignUp from './screens/SignUp';
+import SignIn from './screens/SignIn';
+import ChatRoom from './screens/ChatRoom';
 import styles from './App.styles';
-import { Provider } from 'react-redux'
-import store from './store'
+import { Provider } from 'react-redux';
+import store from './store';
+import { Router, Scene } from 'react-native-router-flux';
 
 export default class ChatApp extends React.Component {
   render() {
     return (
       <Provider store={ store }>
         <View style={styles.container}>
-          <SignUp />
+          <Router>
+            <Scene key="root">
+              <Scene key="signIn" component={ SignIn } title="Sign In" initial={ true } />
+              <Scene key="signUp" component={ SignUp } title="Sign Up" />
+            </Scene>
+          </Router>
         </View>
       </Provider>
     );
